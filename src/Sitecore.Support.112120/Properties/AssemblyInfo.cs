ce46b0259211b4dc32b2fde988457e4dd854bcc1
Dmitry Kolinchuk <dmko@sitecore.net>
2017-05-08 14:05:31 +0300

    Show field title in content language when using Experience Editor

---
+++ src/Sitecore.Support.112120/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.112120")]
+[assembly: AssemblyProduct("Sitecore.Support.112120")]
+[assembly: ComVisible(false)]
